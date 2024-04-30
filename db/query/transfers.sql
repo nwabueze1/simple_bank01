4279743fff72ea470ef39163926097b923625b6b
fidelis okeke <fokeke@wazobia.tech>
2024-04-30 02:41:50 +0100

    entries and transfers crud

---
+++ db/query/transfers.sql
@@ -0,0 +1,34 @@
+-- name: GetTransfer :one
+SELECT * FROM transfers
+WHERE id = $1 LIMIT 1;
+
+-- name: ListTransfers :many
+SELECT * FROM transfers
+ORDER BY id
+LIMIT $1
+OFFSET $2
+;
+
+-- name: ListTransfersFromAccount :many
+SELECT * FROM transfers
+WHERE from_account_id = $1
+ORDER BY id
+LIMIT $2
+OFFSET $3
+;
+
+-- name: ListTransfersToAccount :many
+SELECT * FROM transfers
+WHERE to_account_id = $1
+ORDER BY id
+LIMIT $2
+OFFSET $3
+;
+
+-- name: ListTransfersBetweenAccount :many
+SELECT * FROM transfers
+WHERE to_account_id = $1 AND from_account_id = $2
+ORDER BY id
+LIMIT $3
+OFFSET $4
+;
